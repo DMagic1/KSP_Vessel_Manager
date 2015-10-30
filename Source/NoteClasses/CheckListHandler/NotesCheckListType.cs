f810127477acacd2b91af5a691753ef05e758288
DMagic <david.grandy@gmail.com>
2015-10-30 15:59:04 -0400

    Move to separate file

---
+++ Source/NoteClasses/CheckListHandler/NotesCheckListType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BetterNotes.NoteClasses.CheckListHandler
+{
+	public enum NotesCheckListType
+	{
+		custom = 1,
+		launch = 2,
+		orbit = 3,
+		land = 4,
+		returnHome = 5,
+		rendezvousVessel = 6,
+		dockVessel = 7,
+		rendezvousAsteroid = 8,
+		dockAsteroid = 9,
+		blastOff = 10,
+	}
+}
